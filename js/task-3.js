316645aef78197102465604bfc7463df2368453e
Nazar Pasternak <nazar.pasternakk2001@gmail.com>
2023-12-04 17:56:55 +0200

    6

---
+++ js/task-3.js
@@ -0,0 +1,34 @@
+class StringBuilder {
+  constructor(initialValue) {
+    this.value = initialValue;
+  }
+
+  getValue() {
+    return this.value;
+  }
+
+  padEnd(str) {
+    this.value += str;
+  }
+
+  padStart(str) {
+    this.value = str + this.value;
+  }
+
+  padBoth(str) {
+    this.value = str + this.value + str;
+  }
+}
+
+// Використання класу для перевірки його роботи
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
